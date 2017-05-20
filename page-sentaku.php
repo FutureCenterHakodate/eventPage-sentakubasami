c8b9ac267553c5cf009bf67d63fe9f7a40d749b9
ryu-g <b1013178@fun.ac.jp>
2017-05-20 20:04:00 +0900

    added element

--- page-sentaku.php
+++ page-sentaku.php
@@ -4,14 +4,29 @@
 	<title>Document</title>
 </head>
 
-<header>
+<div>
+<img src="" alt="こどもXせんたくばさみ10kコ+テント6つ=OOの空間">
+</div>
+<div>
+	<img src="" alt="ふせん">
+</div>
+<div>
+<h2>会場</h2>
+<p>XXXXXXXXXXXXXXXXXXXXXX</p>
 
-</header>
-<body>
+<h2>料金</h2>
+<p>XXXXXXXXXXXXXXXXXXXXXX</p>
 
-</body>
+<h2>開催時間</h2>
+<p>XXXXXXXXXXXXXXXXXXXXXX</p>
+
+<h2>休館日</h2>
+<p>XXXXXXXXXXXXXXXXXXXXXX</p>
+</div>
+
+
 
 <footer>
+<small>&copy;2017 Hakodate Mirai Project</small>
 </footer>
-
 </html>
