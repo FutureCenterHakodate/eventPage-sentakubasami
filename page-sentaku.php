7e8b4a11cf87abe413918573cd0e6fec442c2a82
ryu-g <b1013178@fun.ac.jp>
2017-05-21 19:01:10 +0900

    edit some tags and classes
    edit h2->h1
    adding .heading  class
    add tag p

--- page-sentaku.php
+++ page-sentaku.php
@@ -17,48 +17,56 @@
 </div>
 
 <div id="info" class="container">
-	<h3>会場</h3>
-	<p>北海道函館市若松町20番1号</p>
-	<p>キラリス函館3F はこだてみらい館</p>
+	<div class="heading">
+		<h3>会場</h3>
+	</div>
 
-	<h3>料金</h3>
-	<p>大人：600円 中高生：300円 未就学児：無料</p>
-	<p>※3ヶ月、６ヶ月定期券有</p>
-	<p>※２巻共通券有(20名以上の団体：大人480円　中高生：240円)</p>
+	<div class="context">
+		<p>北海道函館市若松町20番1号</p>
+		<p>キラリス函館3F はこだてみらい館</p>
 
-	<h3>開館時間</h3>
-	<p>午前10時〜午後８時</p>
+		<h3>料金</h3>
+		<p>大人：600円 中高生：300円 未就学児：無料</p>
+		<p>※3ヶ月、６ヶ月定期券有</p>
+		<p>※２巻共通券有(20名以上の団体：大人480円　中高生：240円)</p>
 
-	<h3>休館日</h3>
-	<p>毎月第２水曜日</p>
-	<p>※期間中休館は6月14日(水),7月12日(水)です。</p>
+		<h3>開館時間</h3>
+		<p>午前10時〜午後８時</p>
+
+		<h3>休館日</h3>
+		<p>毎月第２水曜日</p>
+		<p>※期間中休館は6月14日(水),7月12日(水)です。</p>
+	</div>
 </div>
 <div id="about" class="container">
-<h2>企画概要</h2>
+	<div class="heading">
+		<h1>企画概要</h1>
+	</div>
 </div>
-
 <div id="event" class="container">
-<h2>イベント</h2>
-アーティストによる洗濯ばさみワークショップ開催！！
-①27日(土) 13:00-14:00, 15:00-16:00, 17:00-18:00の3回実施
-講師：山添joseph勇 氏
-参加費無料(入館料のみ), 申込不要
-
-②28日(日) 13:00-14:00 の1回実施
-講師：カブ 氏
-参加費無料(入館料のみ), 申込不要
-
-※そのほか、展示期間中は洗濯ばさみで遊べます。
-
+	<div class="heading"></div>
+	<h1>イベント</h1>
+	<div class="context">
+	<h3>アーティストによる洗濯ばさみワークショップ開催！！</p3>
+		<p>①27日(土) 13:00-14:00, 15:00-16:00, 17:00-18:00の3回実施</p>
+		<p>講師：山添joseph勇 氏</p>
+		<p>参加費無料(入館料のみ), 申込不要</p>
+		<p>②28日(日) 13:00-14:00 の1回実施</p>
+		<p>講師：カブ 氏</p>
+		<p>参加費無料(入館料のみ), 申込不要</p>
+		<p>※そのほか、展示期間中は洗濯ばさみで遊べます。</p>
+	</div>
 </div>
 <div id="access" class="container">
 	<img class="map" src="http://placehold.jp/800x400.png" alt="">
 </div>
 <div id="contact" class="container">
-<h2>お問い合わせ</h2>
-	TEL:0138-26-6000
-	Eメール：info@hakodate-miraiproject.jp
-	WEBサイト：http://hakodate-miraiproject.jp
+
+	<h1>お問い合わせ</h1>
+
+	<p>TEL:0138-26-6000</p>
+	<p>Eメール：info@hakodate-miraiproject.jp</p>
+	<p>WEBサイト：http://hakodate-miraiproject.jp</p>
 </div>
 <div id="footer" class="container"></div>
 
