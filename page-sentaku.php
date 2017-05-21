db16169c56d7312e73f56d41e7f82d3eda83ee70
ryu-g <b1013178@fun.ac.jp>
2017-05-21 17:42:06 +0900

    built frame

--- page-sentaku.php
+++ page-sentaku.php
@@ -1,16 +1,19 @@
 <html lang="ja">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
+	<title><?php wp_title("はこだてみらい館 |",true);?></title>
+	<link rel="stylesheet" type="text/css" href="http://yui.yahooapis.com/3.18.1/build/cssreset/cssreset-min.css">
+	<link rel="stylesheet" type="text/css" href=<?php echo get_template_directory_uri();?>/css/special.css>
+	<link rel="icon" href=<?php echo get_template_directory_uri();?>/img/favicon.ico>
 </head>
 
-<div>
+<div id="head">
 <img src="" alt="こどもXせんたくばさみ10kコ+テント6つ=OOの空間">
 </div>
-<div>
+<div id="holdtime">
 	<img src="" alt="ふせん">
 </div>
-<div>
+<div id="info" >
 <h2>会場</h2>
 <p>XXXXXXXXXXXXXXXXXXXXXX</p>
 
@@ -23,8 +26,12 @@
 <h2>休館日</h2>
 <p>XXXXXXXXXXXXXXXXXXXXXX</p>
 </div>
+<div class="about"></div>
 
-
+<div class="event"></div>
+<div class="access"></div>
+<div class="contact"></div>
+<div class="footer"></div>
 
 <footer>
 <small>&copy;2017 Hakodate Mirai Project</small>
