52748f445a689852fa9f620a69ec6632e0209d4d
ryu-g <b1013178@fun.ac.jp>
2017-05-21 18:23:26 +0900

    add directory
    and put special.css into it

--- page-sentaku.php
+++ page-sentaku.php
@@ -17,26 +17,42 @@
 </div>
 
 <div id="info" class="container">
-	<h2>会場</h2>
+	<h3>会場</h3>
 	<p>北海道函館市若松町20番1号</p>
 	<p>キラリス函館3F はこだてみらい館</p>
 
-	<h2>料金</h2>
+	<h3>料金</h3>
 	<p>大人：600円 中高生：300円 未就学児：無料</p>
 	<p>※3ヶ月、６ヶ月定期券有</p>
 	<p>※２巻共通券有(20名以上の団体：大人480円　中高生：240円)</p>
 
-	<h2>開館時間</h2>
+	<h3>開館時間</h3>
 	<p>午前10時〜午後８時</p>
 
-	<h2>休館日</h2>
+	<h3>休館日</h3>
 	<p>毎月第２水曜日</p>
 	<p>※期間中休館は6月14日(水),7月12日(水)です。</p>
 </div>
-<div id="about" class="container"></div>
+<div id="about" class="container">
+<h2>企画概要</h2>
+</div>
 
-<div id="event" class="container"></div>
+<div id="event" class="container">
+<h2>イベント</h2>
+アーティストによる洗濯ばさみワークショップ開催！！
+①27日(土) 13:00-14:00, 15:00-16:00, 17:00-18:00の3回実施
+講師：山添joseph勇 氏
+参加費無料(入館料のみ), 申込不要
+
+②28日(日) 13:00-14:00 の1回実施
+講師：カブ 氏
+参加費無料(入館料のみ), 申込不要
+
+※そのほか、展示期間中は洗濯ばさみで遊べます。
+
+</div>
 <div id="access" class="container">
+<h2>アクセス</h2>
 	バス
 	「函館駅前」停留所から徒歩1分
 	「棒二森屋前」停留所から徒歩1分
@@ -49,6 +65,7 @@
 	<img src="http://placehold.jp/600x400.png" alt="">
 </div>
 <div id="contact" class="container">
+<h2>お問い合わせ</h2>
 	TEL:0138-26-6000
 	Eメール：info@hakodate-miraiproject.jp
 	WEBサイト：http://hakodate-miraiproject.jp
