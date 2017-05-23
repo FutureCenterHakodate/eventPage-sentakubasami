7ab8a8a28252a8d8ba923615231f527a880e9b60
ryu-g <b1013178@fun.ac.jp>
2017-05-23 14:40:37 +0900

    fixing about

--- page-sentaku.php
+++ page-sentaku.php
@@ -52,16 +52,18 @@
 
 
 <div id="about">
-	<img src=<?php echo get_template_directory_uri();?>/img/BG2.jpg alt="企画概要BG">
 	<div　class="container">
-		<div class="heading">
-			<h1>企画概要</h1>
+		<div class="aboutBG">
+			<img src=<?php echo get_template_directory_uri();?>/img/BG2.jpg alt="企画概要BG">
+			<div class="aboutAbsolute">
+				<div class="heading">
+					<h1>企画概要</h1>
+				</div>
+				<div class="readAbout">
+					<p>日本、アメリカ、イギリス、マケドニア、インド、イスラエルなどさまざまな国からあつまった『せんたくばさみ』と、キャンプ場のような『テント』のある空間でなにをする？つくる？はさむ？つるす？ならべる？あつめる？かざる？</p>
+				</div>
+			</div>
 		</div>
-		<div class="readAbout">
-			<p>日本、アメリカ、イギリス、マケドニア、インド、イスラエルなどさまざまな国からあつまった『せんたくばさみ』と、キャンプ場のような『テント』のある空間でなにをする？つくる？はさむ？つるす？ならべる？あつめる？かざる？
-			</p>
-		</div>
-
 	</div>
 </div>
 
